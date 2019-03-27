47cbef4600649648c67a011103e02d01ac023966
Adriel Avelar <adriel.avelar@outlook.com.br>
2019-03-27 12:21:49 -0300

    Correções tela layout

--- Adriel.TheMovieDB/Controllers/HomeController.cs
+++ Adriel.TheMovieDB/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
 ﻿using Adriel.TheMovieDB.Domain;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace Adriel.TheMovieDB.Controllers
